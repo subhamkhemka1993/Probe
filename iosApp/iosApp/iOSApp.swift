4f16073e7cdc2c6d37fab39f30f5e70d3d7ccb4c
shubhamkhemka <subham.khemka@dianapps.com>
2026-09-17 23:02:47 +0530

    feat: install probe-runtime from the ios sample app

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,12 @@
 import SwiftUI
+import Shared
 
 @main
 struct iOSApp: App {
+    init() {
+        ProbeBootstrapKt.installProbeSample()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
